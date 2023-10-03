5b359d44b2751d6c091f9a9c130ce609a16ccf93
raviraj <raviraj.mangoIt@gmail.com>
2023-10-03 10:43:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Gallery from "./gallery";
+import SearchForm from "./searchForm";
+import ThemeToggle from "./themeToggle";
+
+function App() {
+  0;
+
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/gallery.jsx
@@ -0,0 +1,58 @@
+import { useQueries, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const url = `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`;
+const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
+  const response = useQuery({
+    queryKey: ["image", searchTerm],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${searchTerm}`);
+      return result.data;
+    },
+  });
+
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>there was an error...</h4>
+      </section>
+    );
+  }
+  const result = response.data.results;
+
+  if (result.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results Found ...</h4>
+      </section>
+    );
+  }
+  return (
+    <section className="image-container">
+      {result.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+export default Gallery;
